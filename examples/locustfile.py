622a576e2240b260b7caef41447359e18bb6aa4b
Mulugeta Ayalew Tamiru <mulugeta.a.tamiru@campus.tu-berlin.de>
2017-12-27 16:29:19 +0100

    Updates, add random

--- examples/locustfile.py
+++ examples/locustfile.py
@@ -1,11 +1,16 @@
 from realbrowserlocusts import HeadlessChromeLocust, PhantomJSLocust
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
+import random
 
 
 from locust import TaskSet, task
 
-baseUrl = "http://35.198.133.209/"
+baseUrl = "http://35.198.145.216/"
+products = ['beanie', 'belt', 'cap', 'sunglasses', 'hoodie-with-logo', 'hoodie-with-pocket', 'hoodie-with-zipper', 'hoodie', 'long-sleeve-tee', 'polo', 'tshirt', 'vneck-tshirt', 'woo-logo', 'woo-album-1', 'woo-album-2', 'woo-album-3', 'woo-single-1', 'woo-album-4', 'woo-single-2', 'woo-logo', 'premium-quality', 'ship-your-idea', 'ninja-silhouette', 'woo-ninja', 'happy-ninja', 'ship-your-idea', 'woo-ninja', 'patient-ninja', 'happy-ninja', 'ninja-silhouette', 'woo-logo', 'flying-ninja', 'premium-quality', 'woo-ninja']
+firstNames = ['Sara', 'Maria', 'Juicy', 'Meron','Christmas','Vanessa']
+lastNames = ['Michaels', 'Jordan', 'Clinton', 'Blunt', 'Harvey', 'Muller', 'Redfern']
+
 class LocustUserBehavior(TaskSet):
 
     def open_locust_homepage(self):
@@ -26,7 +31,12 @@ class LocustUserBehavior(TaskSet):
         )
 
     def make_order_and_checkout(self):
-        self.client.get(baseUrl + "product/beanie/")
+        self.client.get(baseUrl + "product/beanie")
+        self.client.wait.until(
+            EC.visibility_of_element_located(
+                (By.XPATH, '//h1[text()="Beanie"]')
+            )
+        )
         self.client.find_element_by_name('add-to-cart').click()
         self.client.wait.until(
             EC.visibility_of_element_located(
@@ -39,12 +49,12 @@ class LocustUserBehavior(TaskSet):
                 (By.XPATH, '//h1[text()="Checkout"]')
             )
         )
-        self.client.find_element_by_id('billing_first_name').send_keys("Vanessa")
-        self.client.find_element_by_id('billing_last_name').send_keys("Beautiful")
+        self.client.find_element_by_id('billing_first_name').send_keys(random.choice(firstNames))
+        self.client.find_element_by_id('billing_last_name').send_keys(random.choice(lastNames))
         self.client.find_element_by_id('billing_address_1').send_keys("Frankfurter Allee 223")
         self.client.find_element_by_id('billing_postcode').send_keys("10367")
         self.client.find_element_by_id('billing_city').send_keys("Berlin")
-        self.client.find_element_by_id('billing_email').send_keys("vanessabelle19@yahoo.com")
+        self.client.find_element_by_id('billing_email').send_keys("testing@yahoo.com")
         self.client.find_element_by_id('place_order').click()
         self.client.wait.until(
             EC.visibility_of_element_located(
@@ -54,31 +64,35 @@ class LocustUserBehavior(TaskSet):
 
 
     @task(1)
-    def homepage_and_docs(self):
+    def homepage(self):
         self.client.timed_event_for_locust(
             "Go to", "homepage",
             self.open_locust_homepage
         )
+
+    @task(2)
+    def browse_products(self):
         self.client.timed_event_for_locust(
             "Click to",
             "shop",
             self.click_through_to_shop
         )
+
+    @task(3)
+    def add_to_cart_and_checkout(self):
         self.client.timed_event_for_locust(
             "Add",
-            "product",
+            "product and checkout",
             self.make_order_and_checkout
         )
-
-
-
 
 class LocustUser(PhantomJSLocust):
 
     host = "not really used"
-    timeout = 5000  # in seconds in waitUntil thingies
-    min_wait = 100
-    max_wait = 1000
+    timeout = 180  # in seconds in waitUntil thingies
+    stop_timeout = 300
+    min_wait = 0
+    max_wait = 0
     screen_width = 1200
-    screen_height = 600
+    screen_height = 1200
     task_set = LocustUserBehavior
